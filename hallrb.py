cbcc33b2e3816379ae75e4bb21e94cd564213023
dssv <dssv@cin.ufpe.br>
2016-06-25 08:00:51 -0300

    Código Finalizado Contagem BD
    Código com a contagem e envio ao banco de dados implementados e testados

--- hallrb.py
+++ hallrb.py
@@ -1,38 +1,73 @@
 # -*- coding: utf-8 -*-
+
+import MySQLdb as db
 import RPi.GPIO as GPIO
 import time
 
-#pinHallin = 7 # pino do sensor Hall entrada (número GPIO)
-#pinHallout =  # pino do sensor Hall saída
-inputValuein = 1 # valor lido
-inputValueout = 1
-i = 0; # contador entrada
-o = 0; # contador saída
-t = 0; # contador total 
+HOST = '192.168.43.75'
+PORT = 3306
+USER = 'gasj'
+PASSWORD = '123456'
+DB = 'ess'
+num_veiculo = 111 #fixo para um mesmo onibus, funciona como primary key 
+num_linha = 2 #fixo para uma mesma linha
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN)
-GPIO.setup(29, GPIO.IN)
 
-while (1):
-    while (inputValuein and inputValueout) == 1:
-        inputValuein = GPIO.input(7)
-        inputValueout = GPIO.input(29)
-        #print("Contagem de Entrada:")
+def insert_db(cont):
+    try:
+        connection = db.Connection(host=HOST, port=PORT,
+                                   user=USER, passwd=PASSWORD, db=DB)
 
-    if inputValuein == 0:
-        i = i + 1
-        print("Contagem de Entrada:")
-        print(i)
-        time.sleep(0.3)
+        dbhandler = connection.cursor()
+        #sql_st = "INSERT INTO tabela_lot(lot,numveiculo,linha,teste) VALUES(%d,%d,%d,%d)"%(cont,num_veiculo,num_linha,num_veiculo)
+        sql_st = "UPDATE tabela_lot SET lot = %d WHERE teste = %d"%(cont,num_veiculo)
+        dbhandler.execute(sql_st)
+    except Exception as e:
+        print e
 
-    if inputValueout == 0:
-        o = o + 1
-        print("Contagem de Saida:")
-        print(o)
-        time.sleep(0.3)
-        
-    t = i - o
-    print(t)
-    inputValuein = 1
+    finally:
+        connection.commit()
+        connection.close()
+
+def modulo_cont():
+    inputValuein = 1 # valor lido
     inputValueout = 1
+    i = 0; # contador entrada
+    o = 0; # contador saída
+    t = 0; # contador total 
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN)
+    GPIO.setup(29, GPIO.IN)
+
+    while (1):
+        while (inputValuein and inputValueout) == 1:
+            inputValuein = GPIO.input(7)
+            inputValueout = GPIO.input(29)
+            #print("Contagem de Entrada:")
+
+        if inputValuein == 0:
+            i = i + 1
+            print("Contagem de Entrada:")
+            print(i)
+            time.sleep(0.3)
+
+        if inputValueout == 0:
+            o = o + 1
+            print("Contagem de Saida:")
+            print(o)
+            time.sleep(0.3)
+            
+        t = i - o
+        
+        print(t)
+        insert_db(t)
+        inputValuein = 1
+        inputValueout = 1
+
+
+def main():
+    modulo_cont()
+
+if __name__ == '__main__':
+    main()
